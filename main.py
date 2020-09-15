0d5736a2fa08a2e60282d4df02306f0005f3543c
Adam Wilczek <arwilczek90@gmail.com>
2020-09-15 14:38:03 -0500

    Fixed logging, logic and added some env vars to make it adjustable on the fly

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+import os
+import time
+import logging
+
 from kubernetes import client, config
 from datetime import datetime, timedelta
+from kubernetes.client.rest import ApiException
 
 config.load_incluster_config()
-
-v1 = client.CoreV1Api
-
+logging.basicConfig(level=logging.INFO)
+v1 = client.CoreV1Api()
+pod_age = os.environ.get("POD_AGE", "5")
+deployment_name = os.environ.get("DEPLOYMENT_NAME", "kube-prometheas")
 while True:
-    print("Getting Pods")
+    logging.debug("Getting Pods")
     ret = v1.list_pod_for_all_namespaces(watch=False)
     for i in ret.items:
-        if i.start_time > (datetime.now() - timedelta(seconds=60)):
-            print(f"Deleting Pod {i.metadata.name}")
-            v1.delete_namespaced_pod(name=i.metadata.name, namespace=i.metadata.namespace)
+        logging.debug(f"pod_name: {i.metadata.name}")
+        logging.debug(f"startsWith {deployment_name}: {i.metadata.name.startswith(deployment_name)}")
+        if not i.metadata.name.startswith(deployment_name):
+            logging.debug(f"timestamp delta: { (datetime.now() - timedelta(seconds=int(pod_age))).timestamp() - i.metadata.creation_timestamp.timestamp()}")
+            if datetime.utcnow().timestamp() - i.metadata.creation_timestamp.timestamp() > float(pod_age):
+                logging.info(f"Deleting Pod {i.metadata.name}")
+                try:
+                    v1.delete_namespaced_pod(i.metadata.name, i.metadata.namespace)
+                except ApiException:
+                    logging.error(f"Pod not found: {i.metadata.name} :: {i.metadata.namespace}")
+    time.sleep(2)
